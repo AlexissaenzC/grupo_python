f8d96a903cdacdb2b046e37b0954047f20684f73
AlexissaenzC <alexiswuilmersaenzcalvo@gmail.com>
2025-03-15 04:18:32 +0000

    dia 1

---
+++ Semana 1/Ejercicio_0.py
@@ -0,0 +1,6 @@
+a = 3
+b = 1.2
+c = False
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ Semana 1/Ejercicio_1.py
@@ -0,0 +1,32 @@
+#5) Convertir un número entero a flotante y viceversa.
+
+a = 1
+print(type(a))
+a=float(a)
+print(type(a))
+
+#8) Sumar dos números ingresados por el usuario y mostrar el resultado.
+#t = int(input('Ingrese un número: '))
+#r = int(input('Ingrese un número: '))
+#sumar = t+r
+#print(sumar)
+
+a = 5/4
+b = 5//4
+c = 100%7
+print(a)
+print(b)
+print(c)
+
+
+#6) Convertir un número en cadena de texto y viceversa.
+#10) Multiplicar dos números ingresados por el usuario y mostrar el resultado.
+#11) Dividir dos números ingresados por el usuario y mostrar el resultado.
+
+#17) Evaluar la expresión matemática (3 + 5) * 2 - 10 / 2 y mostrar el resultado.
+
+a = (3+5)*2
+b = 10//2
+
+resultado = a-b
+print(resultado)
